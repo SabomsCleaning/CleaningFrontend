eaf4335979320064677c281d352752f9cae468c1
Thomas Hallström <minimoto@hotmail.com>
2025-06-27 23:46:59 +0200

    Adding headline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
     return (
         <div className="bg-purple-100 h-screen flex flex-col justify-center">
+          <div className="m-4 p-4 rounded-2xl text-purple-800 font-extrabold border-2 flex justify-center text-2xl bg-purple-200">Saboms</div>
           <div className="m-4 p-4 rounded-2xl text-white text-2xl bg-purple-800">
             <h1 >
                 Det är här jag kommer testa koden innan vi släpper den i
