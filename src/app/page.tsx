cf0064abcb79c128cad12fcc4cd36a8ea3d30251
Thomas Hallström <minimoto@hotmail.com>
2025-06-27 23:31:56 +0200

    Adding red text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
     <div>detta är startsidan</div>
 <h1>Här kommer lite mer text</h1>
 <h2>Kan det komma ut på nätet?</h2>
+<h3 className="text-red-600">Detta är bara för att testa min kod en gång till</h3>
     </div>
   );
 }
