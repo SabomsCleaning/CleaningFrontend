c7c7462775272c00771c33621dc21a6d57c045f3
Thomas Hallström <minimoto@hotmail.com>
2025-07-02 15:42:30 +0200

    fullösning

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -17,7 +17,8 @@ export async function loginUser(formData: FormData): Promise<LoginResult> {
 
     try {
         const response = await axios.post(
-            `${process.env.NEXT_PUBLIC_API_URL}Auth`,
+            "https://sabomscleaning-dpegfqarf4dxavcc.swedencentral-01.azurewebsites.net/api",
+            //`${process.env.NEXT_PUBLIC_API_URL}Auth`,
             { email, password }, {httpsAgent}
         );
 
