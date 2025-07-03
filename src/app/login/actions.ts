95e7c5e4011ed253526bac542bd29ffd381bf364
MrMinken <minimoto@hotmail.com>
2025-07-03 09:51:12 +0200

    Small change i path

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -8,7 +8,7 @@ import type { LoginResult } from "@/types/loginResult";
 export async function loginUser(formData: FormData): Promise<LoginResult> {
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
-    console.log("URL:", process.env.NEXT_PUBLIC_API_URL);
+    console.log("URL:", process.env.API_URL);
 
     // Denna används för att jag skall kunna köra den utan ett självstängande certifikat
     const httpsAgent = new https.Agent({
