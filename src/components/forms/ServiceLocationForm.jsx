1787a030b6d1e69197f3b2ad5abb2ed99ef766d0
Thomas Hallström <minimoto@hotmail.com>
2025-09-27 12:16:26 +0200

    Fixing in serviceLocation

--- src/components/forms/ServiceLocationForm.jsx
+++ src/components/forms/ServiceLocationForm.jsx
@@ -1,23 +1,100 @@
-import React from 'react'
-import {useForm} from 'react-hook-form'
+"use client";
 
-const ServiceLocationForm = () => {
-    const { register } = useForm();
-  return (
-    <div>
-        <form className='flex flex-col'>
-            <input type="text" placeholder='AddressLine' {...register("AddressLine")} className='input-glow' />
-            <input type="text" placeholder='PostalCode' {...register("PostalCode")} className='input-glow' />
-            <input type="text" placeholder='City' {...register("City")} className='input-glow' />
-            <input type="text" placeholder='Description' {...register("Description")} className='input-glow' />
-            <label>
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
-            <input type="checkbox" {...("IsDefault")} />
-             Isdefault</label>
-             <button type="submit" className='input-glow'>Spara</button>
-        </form>
-    </div>
-  )
-}
+const ServiceLocationForm = ({ service, customerId, setUpdateFlag}) => {
+    const { register, handleSubmit, reset } = useForm();
+    const baseURL = process.env.NEXT_PUBLIC_API_URL;
+    const isUpdate = !!service?.id;
+    const url = isUpdate 
+        ? `${baseURL}/ServiceLocation/${service.id}`
+        : `${baseURL}/ServiceLocation/${customerId}`
 
-export default ServiceLocationForm
+    const saveServiceLocation = async (data) => {
+        console.log(data)
+        const payLoad = {
+            ...data,
+            customerId,
+            serviceLocationId: service?.id
+        };
+
+        console.log(payLoad);
+        const response = await fetch(`${url}`, {
+            method: isUpdate? "PUT": "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify(payLoad),
+        });
+        setUpdateFlag((prev) => !prev)
+        const result = await response.json();
+        console.log(result);
+    };
+
+    const removeServiceLocation = () => {
+        reset({
+                addressLine: "",
+                postalCode: "",
+                city: "",
+                description: "",
+                isDefault: "",
+            })
+    }
+
+    useEffect(() => {
+        if (service) {
+            console.log(service);
+            reset({
+                addressLine: service.address.addressLine,
+                postalCode: service.address.postalCode,
+                city: service.address.city,
+                description: service.description,
+                isDefault: service.isDefault,
+            });
+        }
+    }, [service, reset]);
+    return (
+        <div>
+            <form
+                onSubmit={handleSubmit(saveServiceLocation)}
+                className="flex flex-col">
+                <input
+                    type="text"
+                    placeholder="AddressLine"
+                    {...register("addressLine")}
+                    className="input-glow"
+                />
+                <input
+                    type="text"
+                    placeholder="PostalCode"
+                    {...register("postalCode")}
+                    className="input-glow"
+                />
+                <input
+                    type="text"
+                    placeholder="City"
+                    {...register("city")}
+                    className="input-glow"
+                />
+                <input
+                    type="text"
+                    placeholder="Description"
+                    {...register("description")}
+                    className="input-glow"
+                />
+                <label>
+                    <input type="checkbox" {...register("isDefault")} />
+                    Isdefault
+                </label>
+                <button type="submit" className="input-glow">
+                  {service?.id ? "Uppdatera": "Spara"}  
+                </button>
+                <button type="button" className="input-glow" onClick={() => removeServiceLocation()}>Radera</button>
+            </form>
+        </div>
+    );
+};
+
+export default ServiceLocationForm;
