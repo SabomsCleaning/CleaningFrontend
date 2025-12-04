7be56f5e7f939a179306f91d263c58d0bc5ddcb9
Thomas Hallström <minimoto@hotmail.com>
2025-12-04 11:26:46 +0100

    Update in navbar with swedish name

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -4,11 +4,11 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <div className='flex gap-2'>
-        <Link className='text-purple-800' href="/customer">Customer</Link>
-        <Link className='text-purple-800' href="/dashboard">Dashboard</Link>
-        <Link className='text-purple-800' href="/servicelocation">Service location</Link>
-        <Link className='text-purple-800' href="/servicetype">Service type</Link>
-        <Link className='text-purple-800' href="/booking">Booking</Link>
+        <Link className='text-purple-800' href="/customer">Kunder</Link>
+        <Link className='text-purple-800' href="/dashboard">Schema</Link>
+        <Link className='text-purple-800' href="/servicelocation">Städplats</Link>
+        <Link className='text-purple-800' href="/servicetype">Tjänst</Link>
+        <Link className='text-purple-800' href="/booking">Bokning</Link>
         <Link className='text-purple-800' href="/users">Användare</Link>
     </div>
   )
